58f7f512a6b270b8fdca316b1518f7c52f3ecbf1
Christian Nunciato <chris@nunciato.org>
2025-08-14 17:35:22 -0700

    Change some things

--- apps/web/test/e2e/example.spec.ts
+++ apps/web/test/e2e/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from "@playwright/test";
 test.describe("Layout", () => {
     test("Page title", async ({ page }) => {
         await page.goto("/");
-        await expect(page).toHaveTitle("Astro Stuff");
+        await expect(page).toHaveTitle("Astro Basics");
     });
 });
